66be2931725af7da765f1b0048722b4665204ca2
Mayadev3 <107764065+Mayadev3@users.noreply.github.com>
2023-04-26 15:54:16 +0200

    added picture routes and controllers

--- server/controllers/picture.js
+++ server/controllers/picture.js
@@ -1,6 +1,87 @@
 import Picture from "../models/Picture.js";
 import User from "../models/User.js";
 
-export const getPictures = async (req, res) => {};
+/*READ*/
 
-export const getSpecificPicture = async (req, res) => {};
+export const getPictures = async (req, res) => {
+  try {
+    const pictures = await Picture.find();
+
+    res.status(201).json(pictures);
+  } catch (err) {
+    res.status(404).json({ err: err.message });
+  }
+};
+
+export const getUniquePicture = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const picture = await Picture.findById(id);
+
+    res.status(200).json(picture);
+  } catch (err) {
+    res.status(404).json({ err: err.message });
+  }
+};
+
+/*UPDATE*/
+
+//only by admin
+export const patchUniquePicture = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const picture = await Picture.findById(id);
+
+    const updatedPicture = await Picture.findByIdAndUpdate(
+      id,
+      { title: picture.title },
+      { story: picture.story },
+      { description: picture.description },
+      { price: picture.price },
+      { picturePath: picture.picturePath }
+    );
+    res.send(200).json(updatedPicture);
+  } catch (err) {
+    res.status(404).json({ err: err.message });
+  }
+};
+
+/*DELETE*/
+//only by admin
+export const deletePicture = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const picture = await Picture.findByIdAndDelete(id);
+    res.status(200).json(`Successfully Deleted:`, picture);
+  } catch (err) {
+    res.status(204).json({ err: err.message });
+  }
+};
+
+/*CREATE*/
+
+//only by admin
+
+export const addPicture = async (req, res) => {
+  try {
+    const newPicture = new Picture();
+
+    newPicture.title = req.body.title;
+    newPicture.story = req.body.story;
+    newPicture.description = req.body.description;
+    newPicture.price = req.body.price;
+    newPicture.likes = req.body.likes;
+    newPicture.picturePath = req.body.picturePath;
+    newPicture.comments = req.body.comments;
+
+    await newPicture.save();
+
+    const pictures = await Picture.find();
+    res.status(201).json(pictures);
+  } catch (err) {
+    res.status(409).json({ err: err.message });
+  }
+};
--- server/routes/picture.js
+++ server/routes/picture.js
@@ -1,12 +1,25 @@
 import express from "express";
-import { getPictures, getSpecificPicture } from "../controllers/picture.js";
+import {
+  getPictures,
+  patchUniquePicture,
+  getUniquePicture,
+  deletePicture,
+  addPicture,
+} from "../controllers/picture.js";
 
 const router = express.Router();
 
 /*READ*/
 router.get("/", getPictures);
+router.get("/:id", getUniquePicture);
 
-/*Update*/
-router.patch("/:id", getSpecificPicture);
+/*CREATE*/
+router.post("/", addPicture);
+
+/*DELETE*/
+router.delete("/:id", deletePicture);
+
+/*UPDATE*/
+router.patch("/:id", patchUniquePicture);
 
 export default router;
